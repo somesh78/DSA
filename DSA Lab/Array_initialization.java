cb95fb690d2bef3053e584398038c7a5b742c1c0
somesh78 <rajputsomesh78@gmail.com>
2025-01-08 16:20:39 +0530

    first program

---
+++ DSA Lab/Array_initialization.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Array_initialization {
+    public static void main(String[] args){
+        System.out.println("Enter the sixe of array");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i=0; i<size; i++){
+            System.out.println("Enter the element of" + i + "th index :");
+            arr[i] = sc.nextInt();
+        }
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
---
+++ DSA/src/Sorting/bubblesort.java
@@ -0,0 +1,19 @@
+package Sorting;
+
+public class bubblesort {
+    public static void main(String[] args){
+        int[] arr = {1,5,2,3,4};
+        for(int turns=1; turns<arr.length; turns++){
+            for(int i=0;i<(arr.length-turns);i++){
+                if(arr[i]>arr[i+1]){
+                    int temp = arr[i];
+                    arr[i]=arr[i+1];
+                    arr[i+1]=temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
+
