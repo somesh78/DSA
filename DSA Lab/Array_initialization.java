88a31cd31fa9632a11d2a252c36a887a091bb29e
somesh78 <rajputsomesh78@gmail.com>
2025-01-08 16:35:48 +0530

    .

--- DSA Lab/Array_initialization.java
+++ DSA Lab/Array_initialization.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Array_initialization {
     public static void main(String[] args){
-        System.out.println("Enter the sixe of array");
+        System.out.println("Enter the size of array");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int[] arr = new int[size];
+        int []arr = new int[size];
         for (int i=0; i<size; i++){
-            System.out.println("Enter the element of" + i + "th index :");
+            System.out.println("Enter the element of" + i + " th index :");
             arr[i] = sc.nextInt();
         }
         for (int i=0; i<arr.length; i++){
